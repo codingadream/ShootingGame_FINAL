8a3c1a748d977e11837465d2dc1a212735b94bf4
Liberty Gladden <43799000+codingadream@users.noreply.github.com>
2024-12-17 06:41:06 -0600

    Timer
    Timer

--- Assets/SCRIPTS/ScoreManager.cs
+++ Assets/SCRIPTS/ScoreManager.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
     
     public static ScoreManager sManager;
     public int playerScore = 0;
+    public Text m_Text;
+    public string display = "{0} Score";
+    
+    public Text timerText;
+
+    [SerializeField] float RemainingTime;
     void Start()
     {
         sManager = this;
@@ -16,7 +23,13 @@ public class ScoreManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        m_Text.text = string.Format(display, playerScore.ToString());
+        RemainingTime -= Time.deltaTime;
+        timerText.text = RemainingTime.ToString();
+
+        int minutes = Mathf.FloorToInt(RemainingTime / 60);
+        int seconds = Mathf.FloorToInt(RemainingTime % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 
     public void IncreaseScore(int Increase) { 
